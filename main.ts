935596a34487cffdf67cdd9d33e495740592b87e
HidetoYasuda <73068020+HidetoYasuda@users.noreply.github.com>
2020-11-13 00:43:49 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let reading = 0
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.forever(function () {
+    reading = input.lightLevel()
+    while (input.buttonIsPressed(Button.B)) {
+        led.plotBarGraph(
+        reading,
+        255
+        )
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+    }
+    if (reading < 10) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(1000)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+    }
+})
